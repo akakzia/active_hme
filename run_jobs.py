43a43f1e842be43724eb0bb51079d309e0850337
ahmed <ahmed.akakzia@gmail.com>
2022-07-15 19:39:34 +0200

    Update job runner for main experiments

--- run_jobs.py
+++ run_jobs.py
@@ -13,44 +13,41 @@ scratch = os.environ['SCRATCH']
 # Make top level directories
 mkdir_p(job_directory)
 
-queues = [200]
-betas = [50, 100, 1000]
+betas = [20, 50, 100, 200, 500]
 planning_probas = [0]
 nb_seeds = 3
 
 for s in range(nb_seeds):
-    for pp in planning_probas:
-        for q in queues:
-            for beta in betas:
-                job_file = os.path.join(job_directory, f"exp_queries_q={q}_beta={beta}_pp={pp}%.slurm")
+    for beta in betas:
+        job_file = os.path.join(job_directory, f"main_beta={beta}%.slurm")
 
-                with open(job_file, 'w') as fh:
-                    fh.writelines("#!/bin/bash\n")
-                    fh.writelines("#SBATCH --account=kcr@v100\n")
-                    fh.writelines(f"#SBATCH --job-name=exp_queries_q={q}_beta={beta}_pp={pp}\n")
-                    fh.writelines("#SBATCH --qos=qos_gpu-t3\n")
-                    fh.writelines(f"#SBATCH --output=exp_queries_q={q}_beta={beta}_pp={pp}_%_%j.out\n")
-                    fh.writelines(f"#SBATCH --error=exp_queries_q={q}_beta={beta}_pp={pp}_%_%j.out\n")
-                    fh.writelines("#SBATCH --time=19:59:59\n")
-                    fh.writelines("#SBATCH --ntasks=24\n")
-                    fh.writelines("#SBATCH --ntasks-per-node=1\n")
-                    fh.writelines("#SBATCH --gres=gpu:1\n")
-                    fh.writelines("#SBATCH --hint=nomultithread\n")
-                    fh.writelines("#SBATCH --array=0-0\n")
+        with open(job_file, 'w') as fh:
+            fh.writelines("#!/bin/bash\n")
+            fh.writelines("#SBATCH --account=kcr@v100\n")
+            fh.writelines(f"#SBATCH --job-name=main_beta={beta}\n")
+            fh.writelines("#SBATCH --qos=qos_gpu-t3\n")
+            fh.writelines(f"#SBATCH --output=main_beta={beta}_%_%j.out\n")
+            fh.writelines(f"#SBATCH --error=main_beta={beta}_%_%j.out\n")
+            fh.writelines("#SBATCH --time=19:59:59\n")
+            fh.writelines("#SBATCH --ntasks=24\n")
+            fh.writelines("#SBATCH --ntasks-per-node=1\n")
+            fh.writelines("#SBATCH --gres=gpu:1\n")
+            fh.writelines("#SBATCH --hint=nomultithread\n")
+            fh.writelines("#SBATCH --array=0-0\n")
 
-                    fh.writelines("module load pytorch-gpu/py3/1.4.0\n")
+            fh.writelines("module load pytorch-gpu/py3/1.4.0\n")
 
-                    fh.writelines("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/lib\n")
-                    fh.writelines("export LIBRARY_PATH=$LIBRARY_PATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/lib\n")
-                    fh.writelines("export CPATH=$CPATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/include\n")
-                    fh.writelines("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/linkhome/rech/genisi01/uqy56ga/.mujoco/mujoco200/bin\n")
-                    fh.writelines("export OMPI_MCA_opal_warn_on_missing_libcuda=0\n")
-                    fh.writelines("export OMPI_MCA_btl_openib_allow_ib=1\n")
-                    fh.writelines("export OMPI_MCA_btl_openib_warn_default_gid_prefix=0\n")
-                    fh.writelines("export OMPI_MCA_mpi_warn_on_fork=0\n")
+            fh.writelines("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/lib\n")
+            fh.writelines("export LIBRARY_PATH=$LIBRARY_PATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/lib\n")
+            fh.writelines("export CPATH=$CPATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/include\n")
+            fh.writelines("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/linkhome/rech/genisi01/uqy56ga/.mujoco/mujoco200/bin\n")
+            fh.writelines("export OMPI_MCA_opal_warn_on_missing_libcuda=0\n")
+            fh.writelines("export OMPI_MCA_btl_openib_allow_ib=1\n")
+            fh.writelines("export OMPI_MCA_btl_openib_warn_default_gid_prefix=0\n")
+            fh.writelines("export OMPI_MCA_mpi_warn_on_fork=0\n")
 
-                    fh.writelines(f"srun python -u -B train.py --max-queue-length {q} --beta {beta} --autotelic-planning-proba {pp} --save-dir 'exp_queries_q={q}_beta={beta}/' 2>&1 ")
+            fh.writelines(f"srun python -u -B train.py  --beta {beta} --save-dir 'main_beta={beta}/' 2>&1 ")
 
-                os.system("sbatch %s" % job_file)
-                sleep(1)
+        os.system("sbatch %s" % job_file)
+        sleep(1)
         
